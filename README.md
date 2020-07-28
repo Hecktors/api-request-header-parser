a7f0b539d1dfecf8ae88c8d1254ea447aba270a2
t <hectors.dev@gmail.com>
2020-07-28 06:54:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# API Project: Request Header Parser Microservice for freeCodeCamp
+### User stories:
+Get the IP address, preferred languages and system infos for my device.
+
+#### Usage:
+[bronzed-balanced-toothpaste.glitch.me/api/whoiam](https://bronzed-balanced-toothpaste.glitch.me/api/whoiam)
+
+#### Example output:
+* `{"ipaddress":"159.20.14.100","language":"en-US,en;q=0.5","software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}`
---
+++ server.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const app = express();
+
+var cors = require('cors');
+app.use(cors({ optionSuccessStatus: 200 }));
+
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/api/whoiam", (req, res) => {
+  app.set('trust proxy', true)
+  console.log(req.ips)
+  res.json({ "ipaddress": req.ip, "language": req.headers["accept-language"], "software": req.headers["user-agent"] })
+})
+
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
