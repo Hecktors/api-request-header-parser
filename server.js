b8f4d0372929658ce0f2a02d7287d89ff794a4fb
t <hectors.dev@gmail.com>
2020-07-28 07:03:57 +0200

    fix: route name

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/api/whoiam", (req, res) => {
+app.get("/api/whoami", (req, res) => {
   app.set('trust proxy', true)
   console.log(req.ips)
   res.json({ "ipaddress": req.ip, "language": req.headers["accept-language"], "software": req.headers["user-agent"] })
